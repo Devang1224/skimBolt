c90b27cb87ed24d6b6b5d40a6bcea32474c8e760
Devang1224 <97470914+Devang1224@users.noreply.github.com>
2026-02-16 16:05:51 +0530

    fix: wrong backend url path

--- web/utils/axoisInstance.ts
+++ web/utils/axoisInstance.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 export const api = axios.create({
-    baseURL:  `${process.env.NEXT_PUBLIC_API_URL}/api/v1`,
+    baseURL:  `${process.env.NEXT_PUBLIC_API_URL}`,
     timeout: 1000,
     headers: {'X-Custom-Header': 'foobar'}
   });
